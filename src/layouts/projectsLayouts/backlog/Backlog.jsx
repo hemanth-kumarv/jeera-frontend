eb39793c1c9a1c1115cb0e6e08712639d9fbb136
hemanth-kumarv <hemanth18kumarv@gmail.com>
2022-10-30 00:32:16 +0530

    Add comments to Backlog Screen - issues list filtering logic

--- src/layouts/projectsLayouts/backlog/Backlog.jsx
+++ src/layouts/projectsLayouts/backlog/Backlog.jsx
@@ -11,26 +11,30 @@ const Backlog = (props) => {
         return acc;
     }, []);
 
-    const sprintsCount = 3 + 1;
+    const sprintsCount = 3 + 1; // Number of sprints to be shown + backlog
     const sprints = useMemo(
         () =>
             data
-                ?.filter((issue) => issue.type != "EPIC" && issue.type != "SUBTASK")
+                ?.filter((issue) => issue.type != "EPIC" && issue.type != "SUBTASK") // Backlog does not display Epics and SubTasks
                 ?.reduce((acc, issue, idx, arr) => {
                     let newAcc = [...acc],
-                        currentIdx = Math.floor((idx / arr?.length) * sprintsCount);
-                    let currentSprintData = newAcc[currentIdx];
+                        currentSprintIdx = Math.floor((idx / arr?.length) * sprintsCount); // For sprint number
+
+                    let currentSprintData = newAcc[currentSprintIdx];
                     if (currentSprintData) {
+                        // If data already exists for sprint
                         let issues = [...(currentSprintData.issues || []), issue];
+                        // Add story point type to total for sprint
                         let storyPoints = currentSprintData?.storyPoints;
                         if (issue?.status == "IN_PROGRESS") storyPoints = { ...storyPoints, inProgress: storyPoints?.inProgress + 1 };
                         if (issue?.status == "TODO") storyPoints = { ...storyPoints, notStarted: storyPoints?.notStarted + 1 };
                         if (issue?.status == "DONE") storyPoints = { ...storyPoints, done: storyPoints?.done + 1 };
                         currentSprintData = { ...currentSprintData, issues, storyPoints };
                     } else {
-                        let isBacklog = currentIdx == sprintsCount - 1;
+                        let isBacklog = currentSprintIdx == sprintsCount - 1; // Last sprint automatically becomes backlog
+                        // Default data for new sprint in array
                         currentSprintData = {
-                            name: isBacklog ? "Backlog" : "Jeera Sprint #" + (currentIdx + 1),
+                            name: isBacklog ? "Backlog" : "Jeera Sprint #" + (currentSprintIdx + 1),
                             issues: [issue],
                             sprintStartsOn: new Date(),
                             sprintEndsOn: new Date().setDate(new Date().getDate() + 14),
@@ -39,7 +43,7 @@ const Backlog = (props) => {
                         };
                     }
 
-                    newAcc.splice(currentIdx, 1, currentSprintData);
+                    newAcc.splice(currentSprintIdx, 1, currentSprintData); // Replacing old data with new data
                     return newAcc;
                 }, []),
         [data]
